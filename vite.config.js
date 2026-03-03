3286677a02839641d6dc743a8f07894e0d6eff39
krishna <krishna@xlinq.io>
2026-03-03 14:19:54 +0100

    Fixing vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Replace 'wellness-tracker' below with your actual GitHub repo name
 export default defineConfig({
   plugins: [react()],
   base: '/wellness-tracker/',
